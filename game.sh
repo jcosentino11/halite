839c25514299a17f871f1b51f11863394046ea8f
Joseph Cosentino <jcosentino11@gmail.com>
2017-10-24 20:32:57 -0400

    Starter bot

---
+++ game.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+if [ "$#" -eq 2 ]; then
+	# Have two bots fight each other
+	./halite -d "240 160" "python3 $1.py" "python3 $2.py"
+elif [ "$#" -eq 1 ]; then
+	# Have a bot fight itself
+	./halite -d "240 160" "python3 $1.py" "python3 $1.py"
+else
+	echo "Usage: ./run_game.sh <bot-name> [opponent-bot-name]"
+fi
---
+++ hlt/README.md
@@ -0,0 +1,2 @@
+# halite starter kit
+https://halite.io/learn-programming-challenge/downloads-and-starter-kits/
